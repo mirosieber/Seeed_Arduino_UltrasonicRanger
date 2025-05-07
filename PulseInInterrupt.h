0c82b0cb28d313d89e24cfa872bb5742769208b0
Miro Sieber <160486278+mirosieber@users.noreply.github.com>
2025-05-07 16:52:42 +0200

    Update PulseInInterrupt.h

--- PulseInInterrupt.h
+++ PulseInInterrupt.h
@@ -2,24 +2,27 @@
 #define PULSE_IN_INTERRUPT_H
 
 #include <Arduino.h>
+#include <driver/gpio.h>
 
 class PulseInInterrupt {
 public:
     PulseInInterrupt();
+    ~PulseInInterrupt();
 
-    void begin(uint8_t pin);
+    void init(uint8_t pin);
+    void begin();
     unsigned long getPulseDuration();
 
 private:
-    static void isrWrapper();
+    static void IRAM_ATTR isrHandler(void* arg);
     void handleInterrupt();
 
     uint8_t _pin;
-    static volatile unsigned long _pulseStart;
-    static volatile unsigned long _pulseEnd;
-    static volatile bool _pulseComplete;
-    static volatile bool _waitingForStart;
-    static PulseInInterrupt* _instance;
+    volatile unsigned long _pulseStart;
+    volatile unsigned long _pulseEnd;
+    volatile bool _pulseComplete;
+    volatile bool _waitingForStart;
+    static bool _isrServiceInstalled;
 };
 
-#endif // PULSE_IN_INTERRUPT_H
+#endif

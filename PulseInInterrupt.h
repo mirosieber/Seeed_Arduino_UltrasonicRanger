066a55ad3ab784340c10275e050b3840f38cc3ec
Miro Sieber <160486278+mirosieber@users.noreply.github.com>
2025-05-07 12:49:44 +0200

    Create PulseInInterrupt.h

---
+++ PulseInInterrupt.h
@@ -0,0 +1,25 @@
+#ifndef PULSE_IN_INTERRUPT_H
+#define PULSE_IN_INTERRUPT_H
+
+#include <Arduino.h>
+
+class PulseInInterrupt {
+public:
+    PulseInInterrupt(uint8_t pin);
+
+    void begin();
+    unsigned long getPulseDuration();
+
+private:
+    static void isrWrapper();
+    void handleInterrupt();
+
+    uint8_t _pin;
+    static volatile unsigned long _pulseStart;
+    static volatile unsigned long _pulseEnd;
+    static volatile bool _pulseComplete;
+    static volatile bool _waitingForStart;
+    static PulseInInterrupt* _instance;
+};
+
+#endif // PULSE_IN_INTERRUPT_H

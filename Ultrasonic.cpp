12e4606cda4913ea972757f8f8f10c5154cd75e6
Miro Sieber <160486278+mirosieber@users.noreply.github.com>
2025-05-07 13:55:25 +0200

    Update Ultrasonic.cpp

--- Ultrasonic.cpp
+++ Ultrasonic.cpp
@@ -34,42 +34,19 @@
 #include "Arduino.h"
 #include "Ultrasonic.h"
 
-#ifdef ARDUINO_ARCH_STM32F4
 
-static uint32_t MicrosDiff(uint32_t begin, uint32_t end) {
-    return end - begin;
+Ultrasonic::Ultrasonic() {
 }
 
-static uint32_t pulseIn(uint32_t pin, uint32_t state, uint32_t timeout = 1000000L) {
-    uint32_t begin = micros();
-
-    // wait for any previous pulse to end
-    while (digitalRead(pin)) if (MicrosDiff(begin, micros()) >= timeout) {
-            return 0;
-        }
-
-    // wait for the pulse to start
-    while (!digitalRead(pin)) if (MicrosDiff(begin, micros()) >= timeout) {
-            return 0;
-        }
-    uint32_t pulseBegin = micros();
-
-    // wait for the pulse to stop
-    while (digitalRead(pin)) if (MicrosDiff(begin, micros()) >= timeout) {
-            return 0;
-        }
-    uint32_t pulseEnd = micros();
-
-    return MicrosDiff(pulseBegin, pulseEnd);
-}
-
-#endif // ARDUINO_ARCH_STM32F4
-
-Ultrasonic::Ultrasonic(int pin) {
+void Ultrasonic::init(int pin){
     _pin = pin;
+    pulseSensor.begin(pin);     // Initialisiert Interrupt
 }
 
-long Ultrasonic::duration(uint32_t timeout) {
+    
+
+void Ultrasonic::startMeasurement() {
+    noInterrupts();
     pinMode(_pin, OUTPUT);
     digitalWrite(_pin, LOW);
     delayMicroseconds(2);
@@ -77,28 +54,39 @@ long Ultrasonic::duration(uint32_t timeout) {
     delayMicroseconds(5);
     digitalWrite(_pin, LOW);
     pinMode(_pin, INPUT);
-    long duration;
-    duration = pulseIn(_pin, HIGH, timeout);
-    return duration;
+    interrupts();
+    pulseSensor.begin(_pin);
 }
 
 /*The measured distance from the range 0 to 400 Centimeters*/
-long Ultrasonic::MeasureInCentimeters(uint32_t timeout) {
+long Ultrasonic::getInCentimeters() {
+    unsigned long duration = pulseSensor.getPulseDuration();
+    if (duration > 0) {
+        lastDuration = duration;
+    }
     long RangeInCentimeters;
-    RangeInCentimeters = duration(timeout) / 29 / 2;
+    RangeInCentimeters = lastDuration / 29 / 2;
     return RangeInCentimeters;
 }
 
 /*The measured distance from the range 0 to 4000 Millimeters*/
-long Ultrasonic::MeasureInMillimeters(uint32_t timeout) {
+long Ultrasonic::getInMillimeters() {
+    unsigned long duration = pulseSensor.getPulseDuration();
+    if (duration > 0) {
+        lastDuration = duration;
+    }
     long RangeInMillimeters;
-    RangeInMillimeters = duration(timeout) * (10 / 2) / 29;
+    RangeInMillimeters = lastDuration * (10 / 2) / 29;
     return RangeInMillimeters;
 }
 
 /*The measured distance from the range 0 to 157 Inches*/
-long Ultrasonic::MeasureInInches(uint32_t timeout) {
+long Ultrasonic::getInInches() {
+    unsigned long duration = pulseSensor.getPulseDuration();
+    if (duration > 0) {
+        lastDuration = duration;
+    }
     long RangeInInches;
-    RangeInInches = duration(timeout) / 74 / 2;
+    RangeInInches = lastDuration / 74 / 2;
     return RangeInInches;
 }
